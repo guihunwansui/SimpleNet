d266ce475b8b0b46834648eff7e04b3bc14ce050
guihunwansui <liuxt@umich.edu>
2025-11-20 22:58:30 -0500

    screw

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-datapath=/content/SimpleNet
+datapath=/content/drive/MyDrive
 # datasets=('screw' 'pill' 'capsule' 'carpet' 'grid' 'tile' 'wood' 'zipper' 'cable' 'toothbrush' 'transistor' 'metal_nut' 'bottle' 'hazelnut' 'leather')
 datasets=('screw')
 dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
